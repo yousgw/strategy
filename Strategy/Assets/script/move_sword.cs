f8e44ddfab4b14843e316b17ad6ca1b3e360bc9c
you86 <r115322@g.kobe-kosen.ac.jp>
2019-05-27 10:53:50 +0900

    研究室移行のため環境整備完了まで凍結

---
+++ Strategy/Assets/script/move_sword.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_sword : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+         anim = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.Play();
+        }
+    }
+    
+    void Swing()
+    {
+
+    }
+}
